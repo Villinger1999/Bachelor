92c843ce3b8fb7fed1f7f757571294e8e7cdca91
Villinger1999 <s224227@dtu.dk>
2025-09-24 15:30:04 +0200

    brisque with high quality  image

--- brisque_assesment.py
+++ brisque_assesment.py
@@ -6,6 +6,8 @@ from PIL import Image
 from skimage.metrics import peak_signal_noise_ratio as psnr, structural_similarity as ssim
 import os
 import time
+from test_train_noiseadd import x
+
 
 # Make path the folder path
 path = os.getcwd() + "/"   # always points to the folder you are in
@@ -16,12 +18,14 @@ model = pyiqa.create_metric('brisque', device='cpu')
 results = []
 
 # Create array of image path
-var = ['noisy0_0.jpg', 'noisy0_0.01.jpg', 'noisy0_0.05.jpg', 'noisy0_0.1.jpg', 'noisy0_0.5.jpg']
-var1 = ['noisy1_0.jpg', 'noisy1_0.01.jpg', 'noisy1_0.05.jpg', 'noisy1_0.1.jpg', 'noisy1_0.5.jpg']
+var = ['noisy0_0.jpg', 'noisy0_0.01.jpg', 'noisy0_0.05.jpg', 'noisy0_0.1.jpg', 'noisy0_0.2.jpg', 'noisy0_0.3.jpg', 'noisy0_0.4.jpg', 'noisy0_0.5.jpg']
+var1 = ['noisy1_0.jpg', 'noisy1_0.01.jpg', 'noisy1_0.05.jpg', 'noisy1_0.1.jpg', 'noisy1_0.2.jpg', 'noisy1_0.3.jpg', 'noisy1_0.4.jpg', 'noisy1_0.5.jpg']
+var2 = ['noisy2_0.jpg', 'noisy2_0.01.jpg', 'noisy2_0.05.jpg', 'noisy2_0.1.jpg', 'noisy2_0.2.jpg', 'noisy2_0.3.jpg', 'noisy2_0.4.jpg', 'noisy2_0.5.jpg']
 
 # Reference images
 ref0 = cv2.cvtColor(cv2.imread(path + "data/noise/" + var[0]), cv2.COLOR_BGR2RGB)
 ref1 = cv2.cvtColor(cv2.imread(path + "data/noise/" + var1[0]), cv2.COLOR_BGR2RGB)
+ref2 = cv2.cvtColor(cv2.imread(path + "data/noise/" + var2[0]), cv2.COLOR_BGR2RGB)
 
 def evaluate_set(file_list, ref_img, img_id):
     for fname in file_list:
@@ -43,12 +47,12 @@ def evaluate_set(file_list, ref_img, img_id):
 # Run evaluations
 evaluate_set(var, ref0, "img0")
 evaluate_set(var1, ref1, "img1")
+evaluate_set(var2, ref2, "img2")
 
 # DataFrame
 df_metrics = pd.DataFrame(results).sort_values(by=["Image", "Variance"])
-df_metrics.to_csv(path + "results/assesment/brisque_" + str(time.time_ns()) + ".csv", index=False)
 
-# Plot all metrics vs variance
+# Plot scores vs variance
 metrics = ["BRISQUE", "PSNR", "SSIM"]
 ylabels = {
     "BRISQUE": "Lower=Better",
@@ -68,9 +72,9 @@ for ax, metric in zip(axes, metrics):
     ax.grid(True, linestyle="--", alpha=0.6)
     ax.legend()
 
-plt.suptitle("Image Quality Metrics vs Noise Variance", fontsize=16, y=1.02)
-plt.tight_layout()
-plt.savefig(path + "results/assesment/brisque_results_" + str(time.time_ns()) + ".png", dpi=150)
+plt.suptitle(f"Image Quality Metrics vs Noise Variance res: {x}", fontsize=16, y=1)
+plt.savefig(path + "results/assessment/brisque_results_" + str(time.time_ns()) + ".png", dpi=150)
+df_metrics.to_csv(path + "results/assessment/brisque_" + str(time.time_ns()) + ".csv", index=False)
 
 
 
--- test_train_noiseadd.py
+++ test_train_noiseadd.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 from skimage.metrics import peak_signal_noise_ratio as psnr, structural_similarity as ssim
 import tensorflow as tf
 from skimage import io, util
+from torchvision import transforms
+from PIL import Image
 import os
+import numpy as np
 
 # Make path the folder path
 path = os.getcwd() + "/"   # always points to the folder you are in
@@ -15,24 +18,39 @@ print("Train:", x_train.shape, y_train.shape)
 print("Test:", x_test.shape, y_test.shape)
 
 # Variances for Gaussian noise
-variances = [0, 0.01, 0.05, 0.1, 0.5]
+variances = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+x = 256 # Resolution
+resize = transforms.Resize((x, x))
 
 # Store results
 results = []
+noisy_img = []
+
+image = Image.open('data/hest.jpg')  
+img_resized = resize(image)                  
+img_resized = np.array(img_resized) / 255.0  #normalize
 
 for var in variances:
     noisy0 = util.random_noise(x_train[0], mode='gaussian', var=var)
     noisy1 = util.random_noise(x_train[1], mode='gaussian', var=var)
+    noisy2 = util.random_noise(img_resized, mode='gaussian', var=var) # High resolution image
     
     # Save images
     fname0 = f"noisy0_{var}.jpg"
     fname1 = f"noisy1_{var}.jpg"
+    fname2 = f"noisy2_{var}.jpg"
+    
     io.imsave(path + 'data/noise/' + fname0, (noisy0 * 255).astype("uint8"))
     io.imsave(path + 'data/noise/' + fname1, (noisy1 * 255).astype("uint8"))
+    io.imsave(path + 'data/noise/' + fname2, (noisy2 * 255).astype("uint8")) # High resolution image
+
     
     # Append metadata for DataFrame
     results.append({"Image": "img0", "Variance": var, "Path": fname0, "Array": noisy0})
     results.append({"Image": "img1", "Variance": var, "Path": fname1, "Array": noisy1})
+    results.append({"Image": "img2", "Variance": var, "Path": fname2, "Array": noisy2}) # High resolution image
+
 
 # Convert to DataFrame
 df = pd.DataFrame(results)
@@ -50,3 +68,4 @@ def show_noisy_grid(img_results, title_prefix="Image", save_path=None):
         
 show_noisy_grid(df[df["Image"] == "img0"].to_dict("records"), "Image 0", path + "data/noise/range_noise0.jpg")
 show_noisy_grid(df[df["Image"] == "img1"].to_dict("records"), "Image 1", path + "data/noise/range_noise1.jpg")
+show_noisy_grid(df[df["Image"] == "img2"].to_dict("records"), "Image 2", path + "data/noise/range_noise2.jpg")
