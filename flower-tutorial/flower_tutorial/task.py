dd18fee0428143be79ad7570fa260b4cd5cd3044
Villinger1999 <s224227@dtu.dk>
2025-10-15 14:43:51 +0200

    remove tutorial

--- flower-tutorial/flower_tutorial/task.py
+++ flower-tutorial/flower_tutorial/task.py
@@ -88,38 +88,6 @@ def evaluate(model, dataloader, device='cpu'):
             correct += (predicted == labels).sum().item()                                       # creates a tensor of True/False values for correct predictions, sums and converts that count from a tensor to a Python number.
     return correct / total                                                                      # calculates and returns accuracy
 
-# class Net(nn.Module):
-#     """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
-
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, 10)
-
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, 16 * 5 * 5)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         return self.fc3(x)
-
-
-# fds = None  # Cache FederatedDataset
-
-# pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-
-# def apply_transforms(batch):
-#     """Apply transforms to the partition from FederatedDataset."""
-#     batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
-#     return batch
-
-
 # def load_data(partition_id: int, num_partitions: int):
 #     """Load partition CIFAR10 data."""
 #     # Only initialize `FederatedDataset` once
@@ -138,40 +106,3 @@ def evaluate(model, dataloader, device='cpu'):
 #     trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
 #     testloader = DataLoader(partition_train_test["test"], batch_size=32)
 #     return trainloader, testloader
-
-
-# def train(net, trainloader, epochs, lr, device):
-#     """Train the model on the training set."""
-#     net.to(device)  # move model to GPU if available
-#     criterion = torch.nn.CrossEntropyLoss().to(device)
-#     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-#     net.train()
-#     running_loss = 0.0
-#     for _ in range(epochs):
-#         for batch in trainloader:
-#             images = batch["img"].to(device)
-#             labels = batch["label"].to(device)
-#             optimizer.zero_grad()
-#             loss = criterion(net(images), labels)
-#             loss.backward()
-#             optimizer.step()
-#             running_loss += loss.item()
-#     avg_trainloss = running_loss / len(trainloader)
-#     return avg_trainloss
-
-
-# def test(net, testloader, device):
-#     """Validate the model on the test set."""
-#     net.to(device)
-#     criterion = torch.nn.CrossEntropyLoss()
-#     correct, loss = 0, 0.0
-#     with torch.no_grad():
-#         for batch in testloader:
-#             images = batch["img"].to(device)
-#             labels = batch["label"].to(device)
-#             outputs = net(images)
-#             loss += criterion(outputs, labels).item()
-#             correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-#     accuracy = correct / len(testloader.dataset)
-#     loss = loss / len(testloader)
-#     return loss, accuracy
