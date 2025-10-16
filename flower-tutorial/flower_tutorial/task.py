8f46a610fc7fadcb7a44781a483da49c61ffc7d5
Villinger1999 <s224227@dtu.dk>
2025-10-16 22:09:36 +0200

    dataloadeer for flower

--- flower-tutorial/flower_tutorial/task.py
+++ flower-tutorial/flower_tutorial/task.py
@@ -7,6 +7,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
 import copy
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import IidPartitioner
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Normalize, ToTensor
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -78,21 +82,35 @@ def evaluate(model, dataloader, device='cpu'):
             correct += (predicted == labels).sum().item()                                       # creates a tensor of True/False values for correct predictions, sums and converts that count from a tensor to a Python number.
     return correct / total                                                                      # calculates and returns accuracy
 
-# def load_data(partition_id: int, num_partitions: int):
-#     """Load partition CIFAR10 data."""
-#     # Only initialize `FederatedDataset` once
-#     global fds
-#     if fds is None:
-#         partitioner = IidPartitioner(num_partitions=num_partitions)
-#         fds = FederatedDataset(
-#             dataset="uoft-cs/cifar10",
-#             partitioners={"train": partitioner},
-#         )
-#     partition = fds.load_partition(partition_id)
-#     # Divide data on each node: 80% train, 20% test
-#     partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
-#     # Construct dataloaders
-#     partition_train_test = partition_train_test.with_transform(apply_transforms)
-#     trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
-#     testloader = DataLoader(partition_train_test["test"], batch_size=32)
-#     return trainloader, testloader
+
+fds = None  # Cache FederatedDataset
+
+pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+def apply_transforms(batch):
+    """Apply transforms to the partition from FederatedDataset."""
+    batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+    return batch
+
+
+def load_data(partition_id: int, num_partitions: int):
+    """Load partition CIFAR10 data."""
+    # Only initialize `FederatedDataset` once
+    global fds
+    if fds is None:
+        partitioner = IidPartitioner(num_partitions=num_partitions)
+        fds = FederatedDataset(
+            dataset="uoft-cs/cifar10",
+            partitioners={"train": partitioner},
+        )
+    partition = fds.load_partition(partition_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+    # Construct dataloaders
+    partition_train_test = partition_train_test.with_transform(apply_transforms)
+    trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
+    testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    return trainloader, testloader
+
+## to run $ flwr run . --run-config "num-server-rounds=5 local-epochs=3"
