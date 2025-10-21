af8b004cf6247fe0fcb7e46f6272c866c3ff4db6
Viktor Marius Nysøe Rehling <s224235@dtu.dk>
2025-10-21 14:07:40 +0200

    finished batch norm

--- models/resnet18_V.py
+++ models/resnet18_V.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def batchNorm(input: np.ndarray, step_size:int=1, padding_size:int = 1, kernel_size:int=3):
+
+def batchNorm(input: np.ndarray):
     """Batch normalization
 
     Args:
@@ -18,17 +19,20 @@ def batchNorm(input: np.ndarray, step_size:int=1, padding_size:int = 1, kernel_s
 
     if input.ndim != 3:
         raise ValueError("input must be a 3D array with shape (C, H, W)")
-    
-    padded_input = np.pad(input,pad_width=padding_size,mode='constant')
+
+    input_mean = np.mean(input)
+    input_std = np.std(input)
     
     for c in input[0]:
         for w in input[2]:
             for h in input[1]:
-                h=0
-            
-    return
+                h = (h-input_mean)/input_std
+    return input
     
-def basicBlock(input):
+def basicBlock(input:np.ndarray, step_size:int=1, padding_size:int = 1, kernel_size:int=3):
+    
+    normalized_input = batchNorm(input)
+    padded_input = np.pad(input,pad_width=padding_size,mode='constant')
     
     
     
