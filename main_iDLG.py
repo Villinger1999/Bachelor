4a69ac56560a6459a9f408c999b4db7dda51894d
ViktorNysoe <viktornysoe@hotmail.dk>
2025-11-04 19:37:45 +0100

    Refactor image reconstruction process in main_iDLG.py

--- main_iDLG.py
+++ main_iDLG.py
@@ -7,13 +7,14 @@ import numpy as np
 
 model = get_model()
 
-leaked_grads = torch.load("state_dicts/local_state_client0_pre2.pt")
+leaked_grads = torch.load("state_dicts/local_state_client0_2.pt", weights_only=True)
 
 infered_label = infer_labels_from_bias_grad(leaked_grads, model)
 
-x_shape = [1,3,32,32]
+x_shape = (2,3,32,32)
 
 image_tensor = iDLG(model, leaked_grads, infered_label, x_shape)
 
-save_image(image_tensor, "reconstructions/Infered_image0_pre2.png")
+save_image(image_tensor[0], "reconstructions/Infered_image00_2.png")
+save_image(image_tensor[1], "reconstructions/Infered_image01_2.png")
 print(infered_label)
