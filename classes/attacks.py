25a69b5a12a8431bf0077e90d64b5859882ce341
Nana Villinger <s224227@dtu.dk>
2025-12-03 22:12:09 +0100

    clean up + create classes for modular framework

--- classes/attacks.py
+++ classes/attacks.py
@@ -4,34 +4,36 @@ import torch.nn as nn
 from torchvision import transforms
 from torch.optim.lbfgs import LBFGS
 from PIL import Image
+from classes.defenses import *
 import sys
 
 class iDLG:
     def __init__(
         self,
         model,
-        orig_img,
         label,
-        grads,
-        device,
-        *,
         seed: int,
-        clamp: tuple[float, float]
+        clamp: tuple[float, float],
+        device="cpu",
+        orig_img=None,
+        grads=None,
+        defense=None,
     ) -> None:
         # Respect provided device and keep original dtype of the model/weights
         self.device = device if isinstance(device, str) else (device.type if hasattr(device, "type") else "cpu")
         self.model = model.to(self.device)
-        self.orig_img = orig_img.to(self.device)
         self.criterion = nn.CrossEntropyLoss(reduction='sum').to(self.device)
         self.label = label.to(self.device)
         self.grads = grads
         self.tt = transforms.ToPILImage()
         self.clamp = clamp
-
-        # Align image dtype to model parameter dtype (usually float32)
+        self.defense = defense
         self.param_dtype = next(self.model.parameters()).dtype
-        if self.orig_img.dtype != self.param_dtype:
-            self.orig_img = self.orig_img.to(self.param_dtype)
+        if orig_img is not None:
+            self.orig_img = orig_img.to(self.device)
+            # Align image dtype to model parameter dtype (usually float32)
+            if self.orig_img.dtype != self.param_dtype:
+                self.orig_img = self.orig_img.to(self.param_dtype)
 
         if seed is not None:
             torch.manual_seed(seed)
@@ -40,7 +42,20 @@ class iDLG:
     def attack(self, iterations=200):
         # iDLG training image reconstruction:
         self.model.eval()
+        if self.orig_img is not None and self.grads == None:
+            # compute original gradients
+            predicted = self.model(self.orig_img)
+            loss = self.criterion(predicted, self.label)
+            orig_grads = torch.autograd.grad(loss, self.model.parameters())
+            orig_grads = list((_.detach().clone() for _ in orig_grads))
+        elif self.grads is not None:
+            orig_grads = self.grads
+        else:
+            raise ValueError("orig_img and grads cannot both be None")
         
+        if self.defense != None:
+            orig_grads = self.defense.apply(orig_grads)
+            
         # img_path1 = sys.argv[1]
         # # load and convert to RGB
         # img = Image.open(img_path1).convert("RGB")
@@ -54,8 +69,8 @@ class iDLG:
         dummy_data = (torch.randn(self.orig_img.size(), dtype=self.param_dtype, device=self.device).requires_grad_(True))
 
         # init with ground truth:
-        label_pred = torch.argmin(torch.sum(self.grads[-2], dim=-1), dim=-1).detach().reshape((1,)).requires_grad_(False)
-    
+        label_pred = torch.argmin(torch.sum(orig_grads[-2], dim=-1), dim=-1).detach().reshape((1,)).requires_grad_(False)
+        
         optimizer = LBFGS(
             [dummy_data], lr=.1, max_iter=50,
             tolerance_grad=1e-09, tolerance_change=1e-11,
@@ -72,7 +87,7 @@ class iDLG:
                 dummy_loss = self.criterion(dummy_pred, label_pred)
                 dummy_dy_dx = torch.autograd.grad(dummy_loss, self.model.parameters(), create_graph=True)
                 grad_diff = 0
-                for gx, gy in zip(dummy_dy_dx, self.grads):
+                for gx, gy in zip(dummy_dy_dx, orig_grads):
                     grad_diff += ((gx - gy) ** 2).sum()
                 grad_diff.backward()
                 return grad_diff
---
+++ classes/datasets.py
@@ -0,0 +1,76 @@
+import random
+import torch
+import copy
+import torch.nn as nn
+from torch.utils.data import DataLoader, Subset, random_split
+from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
+
+def get_federated_imagenet(num_clients, batch_size,
+                           train_dir='/dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/val/', 
+                           val_dir='/dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/train/',
+                           train_fraction=0.05, val_fraction=0.005):
+    """
+    Build federated splits for ImageNet using ImageFolder + Subset.
+
+    Returns:
+        client_datasets: list[Dataset] for each client
+        testloader: DataLoader for validation subset
+        num_classes: int
+    """
+    
+    img_tf = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+
+    train_full = ImageFolder(train_dir, transform=img_tf)
+    val_full = ImageFolder(val_dir, transform=img_tf)
+
+    # train subset
+    num_train = len(train_full)
+    train_subset_size = int(train_fraction * num_train)
+    train_indices = random.sample(range(num_train), train_subset_size)
+    trainset = Subset(train_full, train_indices)
+
+    # val/test subset
+    num_val = len(val_full)
+    val_subset_size = int(val_fraction * num_val)
+    val_indices = random.sample(range(num_val), val_subset_size)
+    testset = Subset(val_full, val_indices)
+
+    # split train subset into clients
+    num_samples = len(trainset)
+    client_sizes = [num_samples // num_clients] * num_clients
+    client_sizes[-1] += num_samples - sum(client_sizes)
+    client_datasets = random_split(trainset, client_sizes)
+
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+    num_classes = len(train_full.classes)
+
+    return client_datasets, testloader, num_classes
+
+
+def get_federated_cifar10(num_clients, batch_size, root="./data"):
+    """
+    Simple federated split for CIFAR-10.
+    """
+    tf = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_full = datasets.CIFAR10(root=root, train=True,
+                                  download=True, transform=tf)
+    testset = datasets.CIFAR10(root=root, train=False,
+                               download=True, transform=tf)
+
+    num_samples = len(train_full)
+    client_sizes = [num_samples // num_clients] * num_clients
+    client_sizes[-1] += num_samples - sum(client_sizes)
+    client_datasets = random_split(train_full, client_sizes)
+
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+    num_classes = 10
+
+    return client_datasets, testloader, num_classes
