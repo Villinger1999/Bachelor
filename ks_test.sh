6e74bbab9d0afad9c8ef2e0c0c6d17ee42561e7a
ViktorNysoe <viktornysoe@hotmail.dk>
2025-12-26 15:48:36 +0100

    batch script used to run the expiriment for brisque score

--- ks_test.sh
+++ ks_test.sh
@@ -18,4 +18,14 @@ module load python3/3.12.11
 # source ~/bachelor-env/Scripts/activate
 source /zhome/8e/8/187047/Documents/Bachelor/bachelor-env/bin/activate
 
-python brisque_value.py
+# variance 0.0 and 0.01, reslution inverval [32,96], every forth number in the interval, using 100 images, not saving the plots locally
+python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.01 32 96 4 100 plot
+
+python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.05 32 96 4 100 no_plot
+
+python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.1 32 96 4 100 no_plot
+
+python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.2 32 96 4 100 no_plot
+
+python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.3 32 96 4 100 no_plot
+
