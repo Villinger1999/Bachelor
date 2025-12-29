5cf0bcfed9f8944150cbbfcdd6c87f4b1b9ee12b
ViktorNysoe <viktornysoe@hotmail.dk>
2025-12-29 15:27:52 +0100

    batchscript for the second brisque experiment the Kolmogorov-Smirnov

--- ks_test.sh
+++ ks_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#BSUB -J ks_test_on_brisque_scores
+#BSUB -J brisque_std_01
 #BSUB -q hpc
 #BSUB -W 180
 #BSUB -R "rusage[mem=32G]"
@@ -19,13 +19,22 @@ module load python3/3.12.11
 source /zhome/8e/8/187047/Documents/Bachelor/bachelor-env/bin/activate
 
 # variance 0.0 and 0.01, reslution inverval [32,96], every forth number in the interval, using 100 images, not saving the plots locally
-python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.01 32 96 4 100 plot
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0001
 
-python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.05 32 96 4 100 no_plot
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0004
 
-python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.1 32 96 4 100 no_plot
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0009
 
-python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.2 32 96 4 100 no_plot
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0016
 
-python brisque_value.py /dtu/datasets1/imagenet_object_localization_patched2019/ILSVRC/Data/CLS-LOC/test/ 0.3 32 96 4 100 no_plot
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0025
 
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0036
+
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0049
+
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0064
+
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.0081
+
+python brisque_value.py --plot --image_count 1000 --res_ub 120 --variance 0.01
