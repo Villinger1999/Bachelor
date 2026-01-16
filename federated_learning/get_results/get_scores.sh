8423b510d91b12187099d76749eb3dfd56222df8
Nana Villinger <s224227@dtu.dk>
2026-01-16 13:37:29 +0100

    fix paths

--- federated_learning/get_results/get_scores.sh
+++ federated_learning/get_results/get_scores.sh
@@ -11,22 +11,22 @@
 module load python/3.12.11
 source ~/bachelor-env/bin/activate
 
-python get_scores.py \
-  --csvs results_fl_orig_clip_995.csv \
-  --outdir scores/scores_clip
+python get_results/get_scores.py \
+  --csvs results/scores/results_fl_orig_clip_995.csv \
+  --outdir results/cum_scores/scores_clip
 
-python get_scores.py \
-  --csvs results_fl_orig_sgp_4.csv \
-  --outdir scores/scores_sgp
+python get_results/get_scores.py \
+  --csvs results/scores/results_fl_orig_sgp_4.csv \
+  --outdir results/cum_scores/scores_sgp
 
-python get_scores.py \
-  --csvs results_FL_orig_1.csv results_FL_orig_2.csv results_FL_orig_3.csv results_FL_orig_4.csv results_FL_orig_5.csv\
-  --outdir scores/scores_FL
+python get_results/get_scores.py \
+  --csvs results/scores/results_FL_orig_1.csv results/scores/results_FL_orig_2.csv results/scores/results_FL_orig_3.csv results/scores/results_FL_orig_4.csv results/scores/results_FL_orig_5.csv\
+  --outdir results/cum_scores/scores_FL
 
-python get_scores.py \
-  --csvs results_FL_orig_1_b.csv\
-  --outdir scores
+python get_results/get_scores.py \
+  --csvs results/scores/results_FL_orig_1_b.csv\
+  --outdir results/cum_scores/
 
-python get_scores.py \
-  --csvs results_fl_orig_normclip_9.csv \
-  --outdir scores/norm_clip
+python get_results/get_scores.py \
+  --csvs results/scores/results_fl_orig_normclip_9.csv \
+  --outdir results/cum_scores/norm_clip
